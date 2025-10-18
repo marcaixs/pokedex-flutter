b97643ffedd12719a2ab6fbe4b20053f337db059
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-18 17:54:17 +0200

    feat: create homepage

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Pokedex'), backgroundColor: Colors.red),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Center(
+            child: Image(
+              image: AssetImage('assets/images/professor-oak.png'),
+              height: 500,
+            ),
+          ),
+          SizedBox(
+            width: 200,
+            height: 60,
+            child: ElevatedButton(
+              onPressed: () {
+                
+              },
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.red),
+              ),
+              child: Text(
+                'Enter',
+                style: TextStyle(color: Colors.white, fontSize: 30),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
