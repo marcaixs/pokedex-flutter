4e62c6e12c641690d6ee35a356997fdcbe4b1996
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-18 19:27:51 +0200

    feat: fetch pokemon data and render

--- lib/pages/pokemonlist.dart
+++ lib/pages/pokemonlist.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class PokemonList extends StatefulWidget {
   const PokemonList({super.key});
@@ -8,8 +10,44 @@ class PokemonList extends StatefulWidget {
 }
 
 class _PokemonListState extends State<PokemonList> {
+
+  List _pokemons = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPokemons();
+  }
+
+  void fetchPokemons() async{
+    List pokemons = [];
+
+    for (var i = 1; i <= 10; i++) {
+      final response = await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/$i'));
+      final data = jsonDecode(response.body);
+      print(data['name']);
+      pokemons.add(data);
+      
+    }
+     setState(() {
+        _pokemons = pokemons;
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      body: ListView.builder(
+        itemCount: _pokemons.length,
+        itemBuilder: (context, index){
+          final pokemon = _pokemons[index];
+          return Container(
+            child: Column(
+              children: [Image.network(pokemon['sprites']['front_default']),
+              Text(pokemon['name'])],
+            ),
+          );
+      }),
+    );
   }
 }
