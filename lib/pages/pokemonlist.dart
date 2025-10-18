0a9a35b0ca8226d19c9585d5241d9348f61d903c
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-18 20:02:06 +0200

    style: change gridview layout

--- lib/pages/pokemonlist.dart
+++ lib/pages/pokemonlist.dart
@@ -10,7 +10,6 @@ class PokemonList extends StatefulWidget {
 }
 
 class _PokemonListState extends State<PokemonList> {
-
   List _pokemons = [];
   bool loading = true;
 
@@ -20,37 +19,59 @@ class _PokemonListState extends State<PokemonList> {
     fetchPokemons();
   }
 
-  void fetchPokemons() async{
+  void fetchPokemons() async {
     List pokemons = [];
-    
 
     for (var i = 1; i <= 151; i++) {
-      final response = await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/$i'));
+      final response = await http.get(
+        Uri.parse('https://pokeapi.co/api/v2/pokemon/$i'),
+      );
       final data = jsonDecode(response.body);
       print(data['name']);
       pokemons.add(data);
-      
     }
-     setState(() {
-        _pokemons = pokemons;
-        loading = false;
-      });
+    setState(() {
+      _pokemons = pokemons;
+      loading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: loading ? Center(child: CircularProgressIndicator(),) : ListView.builder(
-        itemCount: _pokemons.length,
-        itemBuilder: (context, index){
-          final pokemon = _pokemons[index];
-          return Container(
-            child: Column(
-              children: [Image.network(pokemon['sprites']['front_default']),
-              Text(pokemon['name'])],
+    return Scaffold(
+      body: loading
+          ? Center(child: CircularProgressIndicator())
+          : GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // number of items in each row
+                mainAxisSpacing: 12.0, // spacing between rows
+                crossAxisSpacing: 12.0, // spacing between columns
+              ),
+              padding: EdgeInsets.all(12.0), // padding around the grid
+              itemCount: _pokemons.length,
+              itemBuilder: (context, index) {
+                final pokemon = _pokemons[index];
+                return Container(
+                  decoration: BoxDecoration(
+                    border: Border.fromBorderSide(
+                      BorderSide(color: Colors.black),
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: const Color.fromARGB(255, 255, 164, 162),
+                  ),
+
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.network(pokemon['sprites']['front_default']),
+                        Text(pokemon['name']),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
-          );
-      }),
     );
   }
 }
