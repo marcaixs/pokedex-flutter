1d27241c3f9c5425f33782ac8a1dfe1eb4611f1c
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-19 19:48:11 +0200

    style: change textheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './pages/homepage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,7 +11,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.pressStart2pTextTheme(),
+      ),
       home: HomePage()
     );
   }
