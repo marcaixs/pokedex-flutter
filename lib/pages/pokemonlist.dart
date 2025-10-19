e423cd9eb5074060df7b33308f5fc5550dbf39a3
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-19 17:05:33 +0200

    feat: add search textfield

--- lib/pages/pokemonlist.dart
+++ lib/pages/pokemonlist.dart
@@ -12,14 +12,24 @@ class PokemonList extends StatefulWidget {
 
 class _PokemonListState extends State<PokemonList> {
   List _pokemons = [];
+  List _filteredPokemons = [];
   bool loading = true;
 
+  //init state calling fetch pokemon
   @override
   void initState() {
     super.initState();
     fetchPokemons();
   }
 
+  //filter pokemon list with user input
+  void filterPokemons(String pokemon){
+    setState(() {
+      _filteredPokemons = _pokemons.where((e)=> e['name'].toString().startsWith(pokemon)).toList();
+    });
+    
+  }
+  //fetch to pokeapi and set state with data
   void fetchPokemons() async {
     List pokemons = [];
 
@@ -28,11 +38,12 @@ class _PokemonListState extends State<PokemonList> {
         Uri.parse('https://pokeapi.co/api/v2/pokemon/$i'),
       );
       final data = jsonDecode(response.body);
-      print(data['name']);
       pokemons.add(data);
     }
+
     setState(() {
       _pokemons = pokemons;
+      _filteredPokemons = _pokemons;
       loading = false;
     });
   }
@@ -46,47 +57,66 @@ class _PokemonListState extends State<PokemonList> {
       ),
       body: loading
           ? Center(child: CircularProgressIndicator())
-          : GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, // number of items in each row
-                mainAxisSpacing: 12.0, // spacing between rows
-                crossAxisSpacing: 12.0, // spacing between columns
-              ),
-              padding: EdgeInsets.all(12.0), // padding around the grid
-              itemCount: _pokemons.length,
-              itemBuilder: (context, index) {
-                final pokemon = _pokemons[index];
-                return InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute<void>(
-                        builder: (context) => PokemonCard(pokemon: pokemon,),
-                      ),
-                    );
-                  },
-
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.fromBorderSide(
-                        BorderSide(color: Colors.black),
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      color: const Color.fromARGB(255, 255, 164, 162),
-                    ),
-                  
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.network(pokemon['sprites']['front_default']),
-                          Text(pokemon['name']),
-                        ],
-                      ),
-                    ),
+          : Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [ 
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: TextField(
+                    decoration: InputDecoration(hintText: 'Search for pokemon...', prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12))),
+                    onChanged: (pokemon){
+                      filterPokemons(pokemon);
+                    },
                   ),
-                );
-              },
+                ),
+                Expanded( //expanded widget to wrap a dynamic widget inside a column
+                  child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2, // number of items in each row
+                        mainAxisSpacing: 12.0, // spacing between rows
+                        crossAxisSpacing: 12.0, // spacing between columns
+                      ),
+                      padding: EdgeInsets.all(12.0), // padding around the grid
+                      itemCount: _filteredPokemons.length,
+                      itemBuilder: (context, index) {
+                        final pokemon = _filteredPokemons[index];
+                        return InkWell(
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute<void>(
+                                builder: (context) => PokemonCard(pokemon: pokemon,),
+                              ),
+                            );
+                          },
+                  
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.fromBorderSide(
+                                BorderSide(color: Colors.black),
+                              ),
+                              borderRadius: BorderRadius.all(Radius.circular(20)),
+                              color: const Color.fromARGB(255, 255, 164, 162),
+                            ),
+                          
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.network(pokemon['sprites']['front_default']),
+                                  Text(pokemon['name']),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                ),
+              ],
             ),
+          ),
     );
   }
 }
